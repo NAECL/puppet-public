105c6e841c8f04b0c4eeecda918b60902d8bd832
Ian Salt <gpl@naecl.co.uk>
2018-08-05 15:34:22 +0100

    Added dummy syslog certs

--- modules/rsyslog/manifests/init.pp
+++ modules/rsyslog/manifests/init.pp
@@ -10,6 +10,37 @@ class rsyslog (
       ensure => present,
     } ->
 
+    file {'/etc/pki/rsyslog':
+      ensure => directory,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0755',
+    } ->
+
+    file {'/etc/pki/rsyslog/ca.pem':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0640',
+      source => 'puppet:///modules/rsyslog/ca.pem',
+    } ->
+
+    file {'/etc/pki/rsyslog/collector-cert.pem':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0640',
+      source => 'puppet:///modules/rsyslog/collector-cert.pem',
+    } ->
+
+    file {'/etc/pki/rsyslog/collector-key.pem':
+      ensure => present,
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0640',
+      source => 'puppet:///modules/rsyslog/collector-key.pem',
+    } ->
+
     file {'/etc/rsyslog.conf':
       owner   => 'root',
       group   => 'root',
