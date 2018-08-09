6959e73682de79b03367b1f471da19e9e8deea25
Ian Salt <gpl@naecl.co.uk>
2018-08-09 11:31:07 +0100

    Set so that default is not to modify rsyslog

--- modules/rsyslog/manifests/init.pp
+++ modules/rsyslog/manifests/init.pp
@@ -1,51 +1,78 @@
 class rsyslog (
-  $role = 'client',
+    $role      = 'none',
+    $collector = 'localhost',
 ) {
     service {'rsyslog':
-      enable => true,
-      ensure => running,
+        enable => true,
+        ensure => running,
     }
 
-    package {'rsyslog-gnutls':
-      ensure => present,
-    } ->
+    if ( $role != 'none' ) {
+        $servername = $base::servername
 
-    file {'/etc/pki/rsyslog':
-      ensure => directory,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0755',
-    } ->
+        package {'rsyslog-gnutls':
+            ensure => present,
+        } ->
 
-    file {'/etc/pki/rsyslog/ca.pem':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0640',
-      source => 'puppet:///modules/rsyslog/ca.pem',
-    } ->
+        file {'/etc/pki/rsyslog':
+            ensure => directory,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0755',
+        } ->
 
-    file {'/etc/pki/rsyslog/collector-cert.pem':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0640',
-      source => 'puppet:///modules/rsyslog/collector-cert.pem',
-    } ->
+        file {'/etc/pki/rsyslog/ca.pem':
+            ensure => present,
+            owner  => 'root',
+            group  => 'root',
+            mode   => '0640',
+            source => 'puppet:///modules/rsyslog/ca.pem',
+        } ->
 
-    file {'/etc/pki/rsyslog/collector-key.pem':
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => '0640',
-      source => 'puppet:///modules/rsyslog/collector-key.pem',
-    } ->
+        file {'/etc/rsyslog.conf':
+            owner   => 'root',
+            group   => 'root',
+            mode    => '0644',
+            content => template('rsyslog/rsyslog.conf.erb'),
+            notify  => Service['rsyslog'],
+        }
 
-    file {'/etc/rsyslog.conf':
-      owner   => 'root',
-      group   => 'root',
-      mode    => '0644',
-      content => template("rsyslog/rsyslog.${role}.conf.erb"),
-      notify  => Service['rsyslog'],
+        if ( $role == 'client' ) {
+            file {'/etc/pki/rsyslog/sender-cert.pem':
+                ensure => present,
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+                notify => Service['rsyslog'],
+                source => [ 'puppet:///modules/rsyslog/sender-cert.pem' ]
+            }
+
+            file {'/etc/pki/rsyslog/sender-key.pem':
+                ensure => present,
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+                notify => Service['rsyslog'],
+                source => [ 'puppet:///modules/rsyslog/sender-key.pem' ]
+            }
+        } else {
+            file {'/etc/pki/rsyslog/collector-cert.pem':
+                ensure => present,
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+                notify => Service['rsyslog'],
+                source => [ 'puppet:///modules/rsyslog/collector-cert.pem' ]
+            }
+
+            file {'/etc/pki/rsyslog/collector-key.pem':
+                ensure => present,
+                owner  => 'root',
+                group  => 'root',
+                mode   => '0640',
+                notify => Service['rsyslog'],
+                source => [ 'puppet:///modules/rsyslog/collector-key.pem' ]
+            }
+        }
     }
 }
