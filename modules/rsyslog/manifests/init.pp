e90f346960841fca5ef0d5b933d799d5f125f7c1
Ian Salt <gpl@naecl.co.uk>
2018-08-09 12:33:01 +0100

    Removed duplicate lines from rsyslog/manifests/init.pp

--- modules/rsyslog/manifests/init.pp
+++ modules/rsyslog/manifests/init.pp
@@ -75,42 +75,4 @@ class rsyslog (
             }
         }
     }
-
-    if ( $role == 'client' ) {
-        file {'/etc/pki/rsyslog/sender-cert.pem':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0640',
-        notify => Service['rsyslog'],
-        source => [ 'puppet:///modules/rsyslog/sender-cert.pem' ]
-        }
-
-        file {'/etc/pki/rsyslog/sender-key.pem':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0640',
-        notify => Service['rsyslog'],
-        source => [ 'puppet:///modules/rsyslog/sender-key.pem' ]
-        }
-    } else {
-        file {'/etc/pki/rsyslog/collector-cert.pem':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0640',
-        notify => Service['rsyslog'],
-        source => [ 'puppet:///modules/rsyslog/collector-cert.pem' ]
-        }
-
-        file {'/etc/pki/rsyslog/collector-key.pem':
-        ensure => present,
-        owner  => 'root',
-        group  => 'root',
-        mode   => '0640',
-        notify => Service['rsyslog'],
-        source => [ 'puppet:///modules/rsyslog/collector-key.pem' ]
-        }
-    }
 }
