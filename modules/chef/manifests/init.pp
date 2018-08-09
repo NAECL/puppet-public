adfde8734143c755a232286cc388aa61421a9577
Ian Salt <gpl@naecl.co.uk>
2018-08-09 21:44:17 +0100

    parameterised chef install

--- modules/chef/manifests/init.pp
+++ modules/chef/manifests/init.pp
@@ -1,10 +1,10 @@
 class chef (
-  $username  = '',
-  $firstname = '',
-  $lastname  = '',
-  $password  = '',
-  $orgname   = '',
-  $email     = '',
+  $username,
+  $firstname,
+  $lastname,
+  $password,
+  $orgname,
+  $email,
 ) {
   class {'chef::install': } ->
   class {'chef::config': }
