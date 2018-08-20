e5b718f8f2e16c29000f44b554b884948405265c
Ian Salt <gpl@naecl.co.uk>
2018-08-20 20:19:48 +0100

    Worked on adding Ubuntu

--- modules/packages/manifests/init.pp
+++ modules/packages/manifests/init.pp
@@ -1,9 +1,23 @@
 class packages {
+    # Common Packages
+
     package {[
-        'bind-utils',
         'telnet',
         'wget',
     ]:
         ensure => present,
     }
+
+    # RedHat Specific Packages
+    if ( $::osfamily == 'redhat' ) {
+        package {[
+            'bind-utils',
+        ]:
+            ensure => present,
+        }
+    }
+
+    # Placeholder for Debian Specific Packages
+    # if ( $::osfamily == 'Debian' ) {
+    # }
 }
