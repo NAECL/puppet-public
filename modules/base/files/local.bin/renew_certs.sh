643b5245f0c4bca87fb9feb79b5478ce488df2c3
Ian Salt <gpl@naecl.co.uk>
2018-11-17 14:03:14 +0000

    Changed renew_certs to find certbot in path

--- modules/base/files/local.bin/renew_certs.sh
+++ modules/base/files/local.bin/renew_certs.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# To get a cert run 
+#
+# certbot --apache certonly
+#
+# Ensure that you only pick one cert at a time
 
 logfile=/var/log/certbot_renew.log.$(date '+%Y%m')
 
 date >> ${logfile} 2>&1
-/bin/certbot renew >> ${logfile} 2>&1
+/usr/bin/env certbot renew >> ${logfile} 2>&1
 echo >> ${logfile} 2>&1
