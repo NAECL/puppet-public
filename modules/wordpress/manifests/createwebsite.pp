a6e64c7bf8d99ae669ce2f7f7dfd8577e77f6a0a
Ian Salt <gpl@naecl.co.uk>
2019-01-01 17:43:08 +0000

    Set to dynamically create watermarks

--- modules/wordpress/manifests/createwebsite.pp
+++ modules/wordpress/manifests/createwebsite.pp
@@ -61,9 +61,9 @@ define wordpress::createwebsite (
   }
 
   exec {"create_/usr/local/buildfiles/$sitename.png":
-    command => "/usr/local/bin/watermark_websites.sh $sitename 1",
+    command => "/usr/local/bin/createWatermarkFile.sh $sitename 1",
     cwd     => '/usr/local/buildfiles',
     creates => "/usr/local/buildfiles/$sitename.png",
-    require => File['/usr/local/bin/watermark_websites.sh'],
+    require => File['/usr/local/bin/createWatermarkFile.sh'],
   }
 }
