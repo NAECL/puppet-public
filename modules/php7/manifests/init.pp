cc164c054ae259bbafb2b8324ccc7d6c3b813832
Ian Salt <ian@naecl.co.uk>
2019-11-03 18:59:54 +0000

    Updated php module

--- modules/php7/manifests/init.pp
+++ modules/php7/manifests/init.pp
@@ -1,6 +1,14 @@
 class php7 (
-    $minor_version = '4',
+  $minor_version = '4',
 ) {
-    # Configure PHP Version
+  # Configure PHP Version
+
+  file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-remi':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0400',
+    source  => 'puppet:///modules/php7/RPM-GPG-KEY-remi',
+  }
 
 }
