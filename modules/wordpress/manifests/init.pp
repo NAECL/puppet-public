d7ecc01477239c96b0cf0b1beb5ef969d09a1e6a
Ian Salt <ian@naecl.co.uk>
2019-11-03 19:01:19 +0000

    Added php7 to wordpress build

--- modules/wordpress/manifests/init.pp
+++ modules/wordpress/manifests/init.pp
@@ -1,6 +1,7 @@
 class wordpress (
   $wordpress_version = '5.0.2',
 ) {
+  class {'php7': } ->
   class {'wordpress::packages': } ->
   class {'wordpress::database': } ->
   class {'wordpress::config': }
