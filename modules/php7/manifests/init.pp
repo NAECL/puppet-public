c12e17139b4ac21e81dbaebe45506b8f53d322d7
Ian Salt <ian@naecl.co.uk>
2019-11-03 19:28:58 +0000

    Moved php packages into php7 class

--- modules/php7/manifests/init.pp
+++ modules/php7/manifests/init.pp
@@ -1,7 +1,8 @@
 class php7 (
   $minor_version = '4',
 ) {
-  # Configure PHP Version
+  # Configure PHP Version - This module will be set to default to the latest as appropriate
+  # however an application can be held back using $minor_version
 
   file {'/etc/pki/rpm-gpg/RPM-GPG-KEY-remi':
     ensure  => present,
@@ -9,11 +10,19 @@ class php7 (
     group   => 'root',
     mode    => '0644',
     source  => 'puppet:///modules/php7/RPM-GPG-KEY-remi',
-  }
+  } ->
 
   file { '/etc/yum.repos.d/remi-php7.repo':
     ensure  => present,
     content => template('php7/remi-php7.repo.erb'),
+  } ->
+
+  package {[
+      'php',
+      'php-xml',
+      'php-mysqlnd',
+    ]:
+    ensure => present,
   }
 
 }
--- modules/wordpress/manifests/packages.pp
+++ modules/wordpress/manifests/packages.pp
@@ -1,14 +1,11 @@
 class wordpress::packages {
 
   package {[
-      'php',
-      'php-xml',
       'httpd',
       'stress',
       'certbot',
       'python2-certbot-apache',
       'ImageMagick',
-      'php-mysqlnd',
       'vsftpd',
     ]:
     ensure => present,
