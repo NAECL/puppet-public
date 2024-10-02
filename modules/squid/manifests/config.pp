326f574950c3a219ad0934dd2d415ae55c484a83
Ian Salt <ian@naecl.co.uk>
2024-10-02 15:45:41 +0100

    Updated squid config from master

--- modules/squid/manifests/config.pp
+++ modules/squid/manifests/config.pp
@@ -1,10 +1,20 @@
 class squid::config (
   $localnet = undef,
+  $userpass = undef,
 ) {
+  file {'/etc/squid/passwords':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'proxy',
+    mode    => '0640',
+    content => template('squid/squid.password.erb'),
+    notify  => Service['squid'],
+  } ->
+
   file {'/etc/squid/squid.conf':
     ensure  => present,
     owner   => 'root',
-    group   => 'squid',
+    group   => 'proxy',
     mode    => '0640',
     content => template('squid/squid.conf.erb'),
     notify  => Service['squid'],
